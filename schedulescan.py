00a1819a0805f932231af1e81a242261b5a10711
arn5891 <118304272+arn5891@users.noreply.github.com>
2025-10-25 15:00:23 -0400

    Add files via upload

--- schedulescan.py
+++ schedulescan.py
@@ -9,23 +9,9 @@ except Exception as e:
     os.system("pip install "+libr)
     python = sys.executable
     os.execv(python, [python] + sys.argv)
-    
 
-class Node:
-    def __init__(self, center, content):
-        self.center = center
-        self.content = content
-
-#filename = fd.askopenfilename(initialdir="/", title="Select a File")
-filename = "C:/Users/gohal/Downloads/E-SPLOST_homeslider_girl-laptop.png"
+filename = fd.askopenfilename(initialdir="/", title="Select a File")
+#filename = "C:/Users/gohal/Downloads/E-SPLOST_homeslider_girl-laptop.png"
 print(filename)
 reader = easyocr.Reader(['en']) # 'en' for English, add other languages as needed
 results = reader.readtext(filename)
-nodes = []
-
-for (bbox, text, prob) in results:
-    c = [(bbox[0][0]-bbox[1][0])/2 , (bbox[0][1]-bbox[3][1])/2]
-    nodes.append(Node(c, text))
-
-
-print(nodes)
